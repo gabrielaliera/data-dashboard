2402e4efabbd630986cb0a66e1c7c932a69da12e
gabrielaliera <gliera2990@gmail.com>
2023-03-28 12:15:53 -0700

    Add image of deck to top row

--- src/Components/dashboard.jsx
+++ src/Components/dashboard.jsx
@@ -1,46 +1,30 @@
 
 import React from 'react';
 import { Component, useState } from 'react'
+import './dashboard.css'
 import Card from './card'
 import List from './list'
 
 const DashBoard = ({deck, cards}) => {
   
-
- // const deckData = data.deck;
- // const cardsData = data.cards;
-  //console.log("deck")
   console.log(deck);
   console.log(cards);
-
-  /*
-  <h2>Dashboard</h2>
-        <p>{deck.data.name}</p>
-        <p>{deck.data.series}</p>
-        <p>{deck.data.releaseDate}</p>
-        <p>{deck.data.total}</p>
-        <img src={data.data.images.logo}></img>
-        
-
-
-       
-      <div className="dashboard-row">
-        <List/>
-      </div>
-      */
+ 
   return (
-    <div>
-      
-      <h2>DashBoard</h2>
-      
+    <div className="App-page">
+      <div className='deckImage'>
+        <img src={deck.images.logo}/>
+      </div>
       <div className="dashboard-row">
-        <img src={deck.images.symbol}></img>
-        <Card name="Name" stat={deck.name}/>
+        <Card name="Symbol" image={deck.images.symbol}/>
         <Card name="Series"stat={deck.series}/>
         <Card name="Release Date" stat={deck.releaseDate}/>
         <Card name="Total Cards" stat={deck.total}/>
       </div>
-      <List cards={cards}/>
+      <div className="dashboard-row">
+        <List cards={cards}/>
+      </div>
+      
     </div>
   )
 }
