aa47b429fddc1520e5a052cca39bb586a62baa6f
gabrielaliera <gliera2990@gmail.com>
2023-06-19 19:02:52 -0700

    Allow for conditional rendering

--- src/Components/pokemonDetails.jsx
+++ src/Components/pokemonDetails.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
-import './card.css';
+import './pokemonDetails.css'
 
 
 const PokemonDetails = ({data}) => {
-  
+   console.log(data);
   return (
     <div className="Details">
-     data?
+     {data ? (
         <div>
-             <h1>{data.name}</h1>
-             <img className="image" src={item.images.small} alt={`Small icon for ${item.name}`}/>
+             <h1><img src={data[0]?.images?.large} alt={`Small icon for ${data[0].name}`}/></h1>
+             <h2>{data[0].flavorText}</h2>
+             
              <table>
                 <tr>
                     <th>HP</th>
@@ -17,27 +18,51 @@ const PokemonDetails = ({data}) => {
                     <th>Rarity</th>
                 </tr>
                 <tr>
-                  <td>{item.hp}</td>
-                  <td>{item.types}</td>  
-                  <td>{item.rarity}</td>
+                  <td>{data[0].hp}</td>
+                  <td>{data[0].types}</td>  
+                  <td>{data[0].rarity}</td>
                 </tr>
              </table>
              <h2>Market Price</h2>
              <table>
                 <tr>
                    <th>Normal Market Price</th>
-                   <th>Holo Market Price</th>
-                   <th>ReverseHolo Foil Market Price</th>
+                   {
+                      data[0]?.tcgplayer?.prices && 'normal' in data[0].tcgplayer.prices ? (
+                        <td>{data[0].tcgplayer.prices.normal.market}</td>
+                      ) : (
+                        <td>"N/A"</td>
+                      )
+                    }
+                   
+                   
                 </tr>
                 <tr>
-                {'normal' in item.tcgplayer.prices ? (<td>{item.tcgplayer.prices.normal.market}</td>) : (<td>"N/A"</td>)}
-                  {'holofoil' in item.tcgplayer.prices ? (<td>{item.tcgplayer.prices.holofoil.market}</td>) : (<td>"N/A"</td>)} 
-                  {'reverseHolofoil' in item.tcgplayer.prices ? (<td>{item.tcgplayer.prices.reverseHolofoil.market}</td>) : (<td>"N/A"</td>)}       
+                  <th>Holo Market Price</th>
+                  {
+                    data[0]?.tcgplayer?.prices && 'holofoil' in data[0].tcgplayer.prices ? (
+                      <td>{data[0].tcgplayer.prices.holofoil.market}</td>
+                    ) : (
+                      <td>"N/A"</td>
+                    )
+                  }
+                </tr>
+                <tr>
+                  <th>ReverseHolo Foil Market Price</th>
+                    {
+                      data[0]?.tcgplayer?.prices && 'reverseHolofoil' in data[0].tcgplayer.prices ? (
+                        <td>{data[0].tcgplayer.prices.reverseHolofoil.market}</td>
+                      ) : (
+                        <td>"N/A"</td>
+                      )
+                    } 
                 </tr>
              </table>      
         </div>
      
-        : <h1>No Data</h1>
+      ) : ( 
+        <h1>No Data</h1>
+      )}
     </div>
   )
 }
