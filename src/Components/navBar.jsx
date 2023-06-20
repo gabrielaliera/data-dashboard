49d0d33d7f0e360fb32a41fb44c70b8bd85b8a3e
gabrielaliera <gliera2990@gmail.com>
2023-06-19 19:41:38 -0700

    Add Links to navBar

--- src/Components/navBar.jsx
+++ src/Components/navBar.jsx
@@ -1,15 +1,15 @@
 import React, { useState } from 'react'
-
+import { Link } from 'react-router-dom';
 
 const NavBar = () => {
  
   return (
-    <div>
-        <ul>
-          <li>DashBoard</li>
-          <li>About</li>
-        </ul>
-    </div>
+    <nav>
+      <div className="side-links">
+        <Link to="/about"><h2>About</h2></Link>
+        <Link to="/"><h2>Crown Zenith Deck</h2></Link>            
+      </div>
+    </nav>
   )
 }
 
