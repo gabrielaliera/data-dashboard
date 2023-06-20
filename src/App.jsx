7f2200083795365e9754e295860947cc56a3ce8b
gabrielaliera <gliera2990@gmail.com>
2023-06-19 19:03:18 -0700

    Combine fetch request and loading

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,14 @@ import { Route, Routes } from "react-router-dom";
 import './App.css'
 import DashBoard from './Components/dashboard';
 import Header from './Components/header';
-import DetailPage from './pages/DetailPage'
+import DetailPage from './Pages/DetailPage'
 
 const API_KEY = import.meta.env.VITE_APP_API_KEY;
 
 function App() {
 
   const [data, setData] = useState({deck:null, cards:null})
- 
+  const [isLoading, setIsLoading] = useState(true);
   
   useEffect ( () => {
     const fetchPokemonData = async () => {
@@ -20,25 +20,24 @@ function App() {
       const setURL = "https://api.pokemontcg.io/v2/" + setOptions 
       const cardURL = "https://api.pokemontcg.io/v2/" +  cardOptions 
       
-      const cardSetResponse = await fetch(setURL,
-                                  {headers: {'x-api-key' : API_KEY}});
-      
-      const cardDataResponse = await fetch (cardURL,
-                                     {headers: {'x-api-key' : API_KEY}});
-      
+      const [cardSetResponse, cardDataResponse] = await Promise.all([
+        fetch(setURL, { headers: { 'x-api-key': API_KEY } }),
+        fetch(cardURL, { headers: { 'x-api-key': API_KEY } }),
+      ]);
+
       const cardSetJson = await cardSetResponse.json();
       const cardDataJson = await cardDataResponse.json();
       
-      
       console.log(cardSetJson.data);
-      console.log("last");
       console.log(cardDataJson.data);
 
       setData({
         deck: cardSetJson.data,
         cards: cardDataJson.data
       });
-      
+     
+      setIsLoading(false);
+    
     };
 
     fetchPokemonData().catch(console.error)
@@ -53,14 +52,18 @@ function App() {
         <Header/>
       </div>
       
-      {data.deck && data.cards ? (
-          <Routes>
-            <Route path="/" element={<DashBoard deck={data.deck} cards={data.cards}/>} />
-            <Route path="/:id" element={<DetailPage data={data}/>} />
-          </Routes> 
-          ) : (
-            <div>Loading...</div>
-          )}
+     
+      {isLoading ? (
+        <div>Loading...</div>
+      ) : (
+        <Routes>
+          <Route
+            path="/"
+            element={<DashBoard deck={data.deck} cards={data.cards} />}
+          />
+          <Route path="/:id" element={<DetailPage data={data.cards} />} />
+        </Routes>
+      )}
       
     </div>
   )
